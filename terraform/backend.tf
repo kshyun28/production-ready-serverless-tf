7fa66015332d176ea887a695d98501b69c91edf8
kshyun28 <jasper.d.gabriel@gmail.com>
2025-01-26 14:56:04 +0800

    build: setup initial terraform project

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "prsls-tf-jasper-state-bucket-ap-southeast-1-499202726088"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    dynamodb_table = "prsls-tf-jasper-terraform-state-lock"
+  }
+}
